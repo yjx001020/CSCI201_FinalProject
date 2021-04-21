50aec5c1fb12d743f802c67ea03b9f4c0fb88f70
Jessie Yang <jiaxiyan@usc.edu>
2021-04-21 21:03:51 +0800

    group update 4/21

---
+++ archive/FinalProject copy.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS FinalProject;
+CREATE DATABASE FinalProject;
+USE FinalProject;
+
+CREATE TABLE Queue (
+  idQueue INT NOT NULL AUTO_INCREMENT,
+  queueStatus VARCHAR(45) NULL,
+  zoomLink VARCHAR(45) NULL,
+  announcement VARCHAR(45) NULL,
+  PRIMARY KEY (idQueue));
+
+CREATE TABLE StudentInQueue (
+  OrderinQueue INT NOT NULL AUTO_INCREMENT,
+  idStudent VARCHAR(45) NULL,
+  studentName VARCHAR(45) NULL,
+  topic VARCHAR(45) NULL,
+  description VARCHAR(45) NULL,
+  PRIMARY KEY (OrderinQueue));
+
+CREATE TABLE Users(
+	idUser INT PRIMARY KEY NOT NULL,
+    UserName VARCHAR(50) NOT NULL,
+    UserPassword VARCHAR(50) NOT NULL,
+    Fname VARCHAR(50) NOT NULL,
+    Lname VARCHAR(50) NOT NULL,
+    isFaculty TINYINT
+);
+Create Table Staff(
+	ID int(11) primary key not null auto_increment,
+    username varchar(50) not null,
+    email varchar(50) not null
+);
+Insert into Staff(username,email) values ('Mike','limike@usc.edu');
+Insert into Staff(username,email) values ('Fiona','mengfeiz@usc.edu');
